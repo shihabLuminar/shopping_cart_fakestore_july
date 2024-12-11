e401b5b85023de85cd4399eda7d0efac72065e5f
Shihab k s <neethu@luminartechnolab.com>
2024-12-11 13:41:31 +0530

    first commit

---
+++ lib/controller/home_screen_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenController with ChangeNotifier {
+  Future<void> getCategories() async {}
+
+  Future<void> getProducts({String? category}) async {}
+}
